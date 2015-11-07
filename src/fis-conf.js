ecf3b8c637089cae64c36bf46d3b475e72067483
剧中人 <bh_lay@126.com>
2015-11-07 17:22:26 +0800

    移除多余的文件release

--- src/fis-conf.js
+++ src/fis-conf.js
@@ -5,6 +5,18 @@ fis.hook('relative');
 fis.match('*.psd',{
 	release: false
 });
+fis.match('gallery.css',{
+	release: false
+});
+fis.match('loading.gif',{
+	release: false
+});
+fis.match('tpl.html',{
+	release: false
+});
+fis.match('view_skin.png',{
+	release: false
+});
 //让所有文件，都使用相对路径。 
 fis.match('**', {
   relative: true
